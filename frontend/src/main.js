d0c282b59923d1bb54ed17e80d37b25023d5fb74
Jean-Code-J <jeanpaul.jimenez@alumnos.ulagos.cl>
2022-11-06 20:14:26 -0300

    Update 06/11

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,6 @@
+//Instancia de aplicación de Vue con la función createApp 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).mount('#app') //.mount() para que se llame a su método, en contenedor app
+
